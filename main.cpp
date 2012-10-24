e3623608baf89a92bac50b2ba58e5f0a5df9e33c
AlexsJones <alexsimonjones@gmail.com>
2012-10-24 11:04:24 +0100

    starting to pull out the specific code for rendering rect shape grid

--- main.cpp
+++ main.cpp
@@ -16,33 +16,45 @@ int main()
 	app.SetFramerateLimit( 60 );
 
 	Quadtree quadtree( 0.0f, 0.0f, 800.0f, 600.0f, 0, 3 );
-
-
 	vector<Object> objects;
 
-	while( app.IsOpened() ) {
+	objects.push_back( Object( 0,0, 32, 32 ) );
+	objects.push_back( Object( 100,100, 32, 32 ) );
+	objects.push_back( Object( 350,350, 32, 32 ) );
+	objects.push_back( Object( 500,500, 32, 32 ) );
+	objects.push_back( Object( 500,510, 32, 32 ) );
+	objects.push_back( Object( 500,520, 32, 32 ) );
+	objects.push_back( Object( 500,525, 32, 32 ) );
+	
+	while( app.IsOpened() ) 
+	{
 		sf::Event event;
 		sf::Vector2f mousePosition = app.ConvertCoords(app.GetInput().GetMouseX(), app.GetInput().GetMouseY());
-		while( app.GetEvent( event ) ) {
-			if ( event.Type == sf::Event::KeyPressed ) {
-				if ( event.Key.Code == sf::Key::Escape ) {
-					app.Close();
-				}
-			}
-			if ( event.Type == sf::Event::MouseButtonPressed ) {
-				objects.push_back( Object( mousePosition.x, mousePosition.y, 32, 32 ) );
-			}
-		}
+		
 		app.Clear();
 
-		for ( int n = 0; n < objects.size(); ++n ) {
+		for ( int n = 0; n < objects.size(); ++n )
+		{
 			quadtree.AddObject( &objects[n] );
 			objects[n].Draw( app );
 		}
-		quadtree.Draw( app );
-
-		vector<Object*> returnObjects = quadtree.GetObjectsAt( mousePosition.x, mousePosition.y );
-		cout << returnObjects.size() << endl;
+		while( app.GetEvent( event ) ) 
+		{
+			if ( event.Type == sf::Event::KeyPressed ) 
+			{
+				if ( event.Key.Code == sf::Key::Escape ) 
+				{
+				app.Close();
+				}
+			}
+			if ( event.Type == sf::Event::MouseButtonPressed ) 
+			{
+				vector<Object*> returnObjects = quadtree.GetObjectsAt( mousePosition.x, mousePosition.y );
+	
+				cout << returnObjects.size() << endl;
+			}
+		}
+							
 		quadtree.Clear();
 
 		app.Display();
